d31b86600aa069e88619eea83db08b90dd9d2606
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-07-15 20:30:07 -0300

    feat: creating auth files with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './module/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './module/auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, AuthModule],
   controllers: [],
   providers: [],
 })
