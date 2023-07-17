cc0e25a8e0f65b7785649e15a9c21b2e023d03aa
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-07-16 23:15:23 -0300

    feat: signing in ok

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { UserRepository } from '../users/repositories/user.repository';
+import { PrismaUserRepository } from '../users/repositories/implementations/prisma-user.repository';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+    PrismaModule,
+    UsersModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    UsersService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
+    },
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
